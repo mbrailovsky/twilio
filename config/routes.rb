a55963d04f4ea5a5ec8cfeffae039d816f4b5795
Alex Duan <alexduan@gmail.com>
2016-07-16 12:39:01 -0400

    updated for custom registrationscontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :catalog_items
-  devise_for :users
+  devise_for :users, controllers: {registrations: :registrations}
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
