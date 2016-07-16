7cf28d5abeb8ed61ce1c8db64a2008744631bbdd
Jitesh <jnadimpalli@gmail.com>
2016-07-16 15:42:04 -0400

    Added order + order items model and added carts + order items controller

---
+++ app/models/order.rb
@@ -0,0 +1,12 @@
+class Order < ApplicationRecord
+  has_many :order_items
+  before_save :update_total
+
+  def total
+    order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
+  end
+
+  def update_total
+    self[:total] = total
+  end
+end
