c4e06c59cf46b63c02b6fd14fddba9eb025a2543
Alex Duan <alexduan@gmail.com>
2016-07-16 13:13:28 -0400

    updated to expose the ability to add more permitted fields

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
+
   layout 'registrations'
+  before_action :configure_permitted_parameters
+
+  protected
+
+  def configure_permitted_parameters
+    # Permit the `subscribe_newsletter` parameter along with the other
+    # sign up parameters.
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:subscribe_newsletter])
+  end
 
 end
