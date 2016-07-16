1af77e832b203dd07d6e37112292c5da3525e4ca
mbrailovsky <mbrailov@Maxims-MacBook-Air.local>
2016-07-16 16:15:32 -0400

    Catalog
    Catalog styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,23 @@
 class HomeController < ApplicationController
+  before_action :set_catalog_item, only: [:show]
 
-
+  # GET /catalog_items
+  # GET /catalog_items.json
   def index
+    @catalog_items = CatalogItem.all
+  end
 
+  # GET /catalog_items/1
+  # GET /catalog_items/1.json
+  def show
+  end
+
+  # GET /catalog_items/new
+  def new
+    @catalog_item = CatalogItem.new
+  end
+
+  # GET /catalog_items/1/edit
+  def edit
   end
 end
