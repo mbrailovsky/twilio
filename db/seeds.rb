20756be655420dffda071063a7540f3e9f82cf7c
kohnishi <ken.ohnishi@dynamotechnologies.com>
2016-07-16 12:31:12 -0400

    seed catalog items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+catalogItems = CatalogItem.create([
+                                      { name: 'Item1' , price: 1000},
+                                      { name: 'Item2' , price: 2000},
+                                      { name: 'Item3' , price: 3000},
+                                      { name: 'Item4' , price: 4000},
+                                      { name: 'Item5' , price: 5000},
+                                      { name: 'Item6' , price: 6000},
+                                      { name: 'Item7' , price: 7000},
+                                      { name: 'Item8' , price: 8000},
+                                      { name: 'Item9' , price: 9000},
+                                      { name: 'Item10' , price: 10000},
+                                  ])
