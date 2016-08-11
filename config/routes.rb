68c8e1dcda6acff4d7c82f69e156bd974fd7be68
jnadimpalli <jnadimpalli@gmail.com>
2016-08-11 08:36:35 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
 
   get '/health_check', to: redirect('/health_check.html')
   resources :security_questions
-  resources :order_items
   resources :orders
   resources :catalog_items
   resource :cart, only: [:show]
-  resources :order_items, only: [:create, :update, :destroy]
+  resources :order_items
 
   devise_for :users, controllers: {registrations: 'registrations'}
   root to: "home#index"
