4d9e775b9d0d50b501b45adb49f53494ca0f1188
jnadimpalli <jnadimpalli@gmail.com>
2016-08-12 19:06:54 -0400

    simplified order items model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,18 @@
 class OrderItem < ApplicationRecord
   belongs_to :catalog_item
-  belongs_to :carts
+  belongs_to :order
 
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  def unit_price
+    if persisted?
+      self[:unit_price]
+    else
+      errors.add(:unit_price, "is not valid or is not active.")
+    end
+  end
+
+  def total_price
+    unit_price * quantity
+  end
 end
